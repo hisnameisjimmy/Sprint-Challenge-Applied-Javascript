8dac76fd43eeedab5de0c5ade516fd275f2ec10c
Jimmy Hooker <jimmyjjwalker@gmail.com>
2019-09-15 19:24:58 -0700

    Finished tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,57 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+// const topicList = () => {
+//     axios.get('https://lambda-times-backend.herokuapp.com/topics')
+//         .then((results) => {
+//             // console.log(results.data.topics);
+//             // return results.data.topics;
+//             results.data.topics.forEach((topic) => {
+//                 tabElement.appendChild(tabs(topic));
+//             });
+//         });
+// }
+
+// const topicList = () => {
+//     let arrayResults = [];
+//     axios.get('https://lambda-times-backend.herokuapp.com/topics')
+//         .then((results) => {
+//             console.log(results.data.topics);
+//             array.push(results.data.topics);
+//         });
+//         return arrayResults;
+// }
+
+let topicListArray = [];
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then((results) => {
+        // console.log(results.data.topics);
+        // return results.data.topics;
+        results.data.topics.forEach((topic) => {
+            // tabElement.appendChild(tabs(topic));
+            topicListArray.push(topic);
+        });
+    })
+    .then(() => {
+        const tabElement = document.querySelector('.topics');
+
+        topicListArray.forEach((topic) => {
+            tabElement.appendChild(tabs(topic));
+        });
+    });
+
+
+const tabs = (tabData) => {
+    const tab = document.createElement('div');
+
+    tab.classList.add('tab');
+
+    tab.textContent = tabData;
+
+    return tab;
+}
+
+
+
